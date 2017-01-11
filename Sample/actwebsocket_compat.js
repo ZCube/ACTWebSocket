67c706aac3be241f529fa4b1cad625bd25e2a663
ZCube <zcube@zcube.kr>
2017-01-11 21:40:09 +0900

    compat js 추가.

---
+++ Sample/actwebsocket_compat.js
@@ -0,0 +1,38 @@
+/* ACTWebSocket  Begin */
+
+class WebSocketImpl extends ActWebsocketInterface
+{
+  constructor(uri, path = "MiniParse") {
+      super(uri, path);
+  }
+  //send(to, type, msg)
+  //broadcast(type, msg)
+  onRecvMessage(e)
+  {
+  }
+
+  onBroadcastMessage(e)
+  {
+      if (e.detail.msgtype == "CombatData")
+      {
+          document.dispatchEvent(new CustomEvent('onOverlayDataUpdate', { detail: e.detail.msg }));
+      }
+  }
+};
+
+var webs = null;
+var wsUri = "ws://@HOST_PORT@/MiniParse";
+$(document).ready(function() {
+  webs = new WebSocketImpl(wsUri);
+  webs.connect();
+});
+if (document.addEventListener) {
+  window.onbeforeunload = function() {
+      webs.close();
+  };
+  window.addEventListener("unload", function() {
+      webs.close();
+  }, false);
+}
+
+/* ACTWebSocket  End */
