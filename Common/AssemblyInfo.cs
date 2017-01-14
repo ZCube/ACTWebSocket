751d297a3924001701fd5c395e532b4ff4808178
ZCube <zcube@zcube.kr>
2017-01-14 13:31:34 +0900

    1.1.0 release.

---
+++ Common/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0-dev")]
---
+++ Common/setdirty.ps1
@@ -0,0 +1,4 @@
+$a = cmd /c git describe
+(Get-Content AssemblyInfo.cs.in) | ForEach-Object { $_ -replace "@VERSION_SHORT@", $a } | Set-Content AssemblyInfo.cs
+$a = cmd /c git describe --dirty=-dev
+(Get-Content AssemblyInfo.cs) | ForEach-Object { $_ -replace "@VERSION_LONG@", $a } | Set-Content AssemblyInfo.cs
