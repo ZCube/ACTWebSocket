99cdc7ec4e60cd327ac71d9d68d554e913431917
ZCube <zcube@zcube.kr>
2017-05-09 03:30:33 +0900

    wsUri 체크 개선.

--- Sample/actwebsocket.js
+++ Sample/actwebsocket.js
@@ -30,38 +30,58 @@ var QueryString = function ()
 
 // webs
 var host_port = QueryString["HOST_PORT"];
+var need_to_get_host_port = false;
+var need_to_set_wsuri = (typeof wsUri === "undefined");
 
-var wsUri = "@HOST_PORT@/MiniParse"; /*DO NOT EDIT THIS VALUE*/
-
-while(host_port.endsWith('/')) {
-	host_port = host_port.substring(0, host_port.length - 1);
-}
-
-if(wsUri.indexOf("//") == 0) {
-	wsUri = wsUri.substring(2);
-}
-
-if(wsUri.indexOf("ws://") == 0 || wsUri.indexOf("wss://") == 0)
+if (need_to_set_wsuri)
 {
-	if(host_port.indexOf("ws://") == 0 || host_port.indexOf("wss://") == 0)
-	{
-		wsUri = wsUri.replace(/ws:\/\/@HOST_PORT@/im, host_port);
-		wsUri = wsUri.replace(/wss:\/\/@HOST_PORT@/im, host_port);
-	}
-	else
-	{
-		wsUri = wsUri.replace(/@HOST_PORT@/im, host_port);
-	}
+	window.wsUri = "ws://@HOST_PORT@/MiniParse";
+	need_to_get_host_port = typeof host_port === "undefined";
 }
 else
 {
-	if(host_port.indexOf("ws://") == 0 || host_port.indexOf("wss://") == 0)
+	need_to_get_host_port = (typeof host_port === "undefined") ? wsUri.indexOf('HOST_PORT') === -1 : true;
+}
+
+if(need_to_get_host_port)
+{
+	// ws://localhost:10501/
+	host_port = 'ws://' + window.location.host + '/';
+}
+
+// wsUri check
+if (wsUri.indexOf('@HOST_PORT') !== -1)
+{
+	while(host_port.endsWith('/')) {
+		host_port = host_port.substring(0, host_port.length - 1);
+	}
+
+	if(wsUri.indexOf("//") == 0) {
+		wsUri = wsUri.substring(2);
+	}
+
+	if(wsUri.indexOf("ws://") == 0 || wsUri.indexOf("wss://") == 0)
 	{
-		wsUri = wsUri.replace(/@HOST_PORT@/im, host_port);
+		if(host_port.indexOf("ws://") == 0 || host_port.indexOf("wss://") == 0)
+		{
+			wsUri = wsUri.replace(/ws:\/\/@HOST_PORT@/im, host_port);
+			wsUri = wsUri.replace(/wss:\/\/@HOST_PORT@/im, host_port);
+		}
+		else
+		{
+			wsUri = wsUri.replace(/@HOST_PORT@/im, host_port);
+		}
 	}
 	else
 	{
-		wsUri = "ws://" + wsUri.replace(/@HOST_PORT@/im, host_port);
+		if(host_port.indexOf("ws://") == 0 || host_port.indexOf("wss://") == 0)
+		{
+			wsUri = wsUri.replace(/@HOST_PORT@/im, host_port);
+		}
+		else
+		{
+			wsUri = "ws://" + wsUri.replace(/@HOST_PORT@/im, host_port);
+		}
 	}
 }
 
@@ -72,7 +92,7 @@ class ActWebsocketInterface
 		var querySet = this.getQuerySet();
 		if(typeof querySet["HOST_PORT"] != 'undefined')
 		{
-		    uri = querySet["HOST_PORT"] + path;
+				uri = querySet["HOST_PORT"] + path;
 		}
 		this.uri = uri;
 		this.id = null;
